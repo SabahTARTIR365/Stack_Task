bd58f52982233519a28b2739c39d77eebd6c9076
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-02-24 15:51:15 +0200

    some change in main

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace StackTask
     class Program
     {
         static void Main(string[] args)
-        {  double sum = 0.0;
+        {  //double sum = 0.0;
             Console.WriteLine("Hello World!");
             var stack = new STACK<double>();
             stack.Push(1.5);
@@ -12,14 +12,9 @@ namespace StackTask
             stack.Push(5.2);
             stack.Print();
             stack.Peak();
-            while (stack.Count>0)
-            {
-              double item = stack.Pop();
-              System.Console.WriteLine($"Item: {item}");
-              sum += item;
-            }
-              System.Console.WriteLine($"sum{sum}");
-               stack.Peak();
+            stack.Clear();
+             // System.Console.WriteLine($"sum{sum}");
+             stack.Peak();
         }
     }
 }
