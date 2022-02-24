3f41d729b2c3715de3ed4121f371f8db1ce8aaa9
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-02-24 13:40:47 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace StackTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  double sum = 0.0;
+            Console.WriteLine("Hello World!");
+            var stack = new STACK<double>();
+            stack.Push(1.5);
+            stack.Push(37.5);
+            stack.Push(5.2);
+            stack.Print();
+            stack.Peak();
+            while (stack.Count>0)
+            {
+              double item = stack.Pop();
+              System.Console.WriteLine($"Item: {item}");
+              sum += item;
+            }
+              System.Console.WriteLine($"sum{sum}");
+               stack.Peak();
+        }
+    }
+}
---
+++ STACK.cs
@@ -0,0 +1,35 @@
+using System;
+namespace StackTask
+{
+    public class STACK <T>
+    {
+        private readonly T[] _items;
+        private int _currentIndex=-1;
+
+        public STACK() =>_items = new T[1000000];
+       public int Count=> _currentIndex +1; 
+        public void Push(T item)=> _items[++_currentIndex]=item;
+      
+        // it's a return statment indirictly
+          public T Pop()=>  _items[_currentIndex--];
+          public void Print ()
+          {
+              System.Console.WriteLine("[");
+            for(int i=0; i<=_currentIndex;i++) System.Console.WriteLine($"{_items[i]},");
+              System.Console.WriteLine("]");
+          }
+           public void Peak ()
+           {
+               if (_currentIndex<0) {System.Console.WriteLine("The stack is empty");}
+               else { System.Console.WriteLine(_items[_currentIndex]);}
+
+           }
+            public void Clear()
+            {   if (_currentIndex<0) {System.Console.WriteLine("The stack is empty");}
+                 else{ while (Count>0)  { T item = Pop();
+              System.Console.WriteLine($"Item removed: {item}");}}
+            }
+
+        
+    }
+}
