b34e39bbe4170816473bc7eb5b33735a11e18261
SabahTARTIR365 <sosotarteer2017@gmail.com>
2022-02-26 15:50:13 +0200

    only test

--- STACK.cs
+++ STACK.cs
@@ -5,9 +5,11 @@ namespace StackTask
     {   private readonly T[] _items;
         private int _currentIndex=-1;
 
+
         public STACK() =>_items = new T[1000000];
        public int Count=> _currentIndex +1; 
         public void Push(T item)=> _items[++_currentIndex]=item;
+      
       
         // it's a return statment indirictly
           public T Pop()=>  _items[_currentIndex--];
